afe98bf00fa79d79993135d8635a1c5f751bcf18
phakamani1 <mkulisep@gmail.com>
2021-07-12 21:43:45 +0200

    Added server request and response

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -27,9 +29,11 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     MatInputModule,
     MatButtonModule,
     MatCardModule,
-    MatToolbarModule
+    MatToolbarModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
+import { User } from './../../model/user.model';
+import { LoginService } from './login.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  response: Response;
+  constructor(
+    private formBuilder: FormBuilder,
+    private loginService: LoginService
+  ) { }
 
   ngOnInit(): void {
+    this.createForm();
   }
 
+  createForm () {
+    this.loginForm = this.formBuilder.group({
+      userName: this.formBuilder.control(''),
+      password: this.formBuilder.control(
+        ''
+      )
+    })
+  }
+
+  loginValidator(control: FormControl) {
+    return null
+  }
+
+  onSubmit() {
+    const user: User = {
+      userName: this.loginForm.controls['userName'].value,
+      password: this.loginForm.controls['password'].value
+    };
+
+    this.loginService.login(user).subscribe((res) => {
+      if (res.status === 404) {
+        this.response = res.error;
+      }
+      this.response = res;
+    }, error => {
+      this.response = error.error;
+    });
+  }
 }
