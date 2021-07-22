d617a5868e56f1e68cb644bda3dd1076374753ad
phakamani1 <mkulisep@gmail.com>
2021-07-23 00:14:52 +0200

    Added idea image

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
 
 const routes: Routes = [
   {
